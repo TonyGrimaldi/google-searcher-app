047fc49c4d9310b593be3d27688b99237a1c08f4
tony <antoninogrimaldi14@gmail.com>
2018-08-20 20:22:05 +0200

    added google KEY the customs researches and setted a first try-research

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,32 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+
+const URL = 'https://www.googleapis.com/customsearch/v1?';
+const key = 'AIzaSyAZzAPJwFLT6VuWqo03IHdJ86K7ndGAET4';
+const cx = '000674345227953438871:y-whkppmv8k';
+const query = 'juventus';
+const fullURL = `${URL}key=${key}&cx=${cx}&q=${query}`;
+
+
+
 
 export default class App extends Component {
+
+  state = {
+    results: []
+  }
+
+  componentDidMount() {
+    axios.get(fullURL)
+      .then(res => {
+        const results = res.data;
+        this.setState({ results });
+      })
+  }
+
   render() {
     return (
-      <div>React simple starter</div>
+      <div>{console.log(this.state)}</div>
     );
   }
 }
